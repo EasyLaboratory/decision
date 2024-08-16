17af09587d2bc7f750eefd52f97c4464c4bc47ab
wuyu <bupt_wuyu@163.com>
2024-08-16 17:08:31 +0800

    update

---
+++ src/decision.cpp
@@ -0,0 +1,17 @@
+// #include"ros/ros.h"
+
+// void detectionCallback(){
+//   ROS_INFO("hello");
+// };
+
+// int main(int argc, char **argv)
+// {
+//     ros::init(argc, argv, "perception_subscriber");
+//     ros::NodeHandle n;
+
+//     ros::Subscriber sub = n.subscribe("perception_detections", 1000, detectionCallback);
+
+//     ros::spin();
+
+//     return 0;
+// }
